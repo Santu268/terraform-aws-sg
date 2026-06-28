91f916c897080101f88d6293d3a458791ce29843
Santosh <rampalli268@gmail.com>
2026-06-28 14:32:44 +0530

    sg module

--- variables.tf
+++ variables.tf
@@ -17,5 +17,5 @@ variable "sg_tags"{
 
 variable "sg_names"{
     type = list
-    default = [mongodb,redis,rabbitmq,mysql,catalogue,cart,user,payment,shipping,frontend,backend_alb,frontend_alb,bastion]
+    default = ["mongodb","redis","rabbitmq","mysql","catalogue","cart","user","payment","shipping","frontend","backend_alb","frontend_alb","bastion"]
 }
